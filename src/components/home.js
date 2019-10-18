82043c77ff70c715c1230700f8df0f5009177900
Matt Nickel <matt@nickelimages.com>
2019-10-18 13:12:47 -0700

    text update

--- src/components/home.js
+++ src/components/home.js
@@ -23,8 +23,8 @@ export default class Home extends React.Component {
             I'm creative, enjoy people, and thrive on getting things done. I have a passion to build the right things - the things your user really wants. This involves empathetic listening to actual users, stakeholders, and developers.</p>
           <p className={["seven", "columns", "about-message"].join(' ')}> I want to work with a team to <b>create products </b>that delight the users. From user research and wireframes to design prototypes and software development... I have done it all. I have a proven ability to <b>connect with customers </b> and develop strategic <b>technological solutions</b> to real-word problems. </p>
           <p className={["seven", "columns", "about-message"].join(' ')}>I have had the opportunity to make an impact in a variety of roles including software developer, product manager, sales trainer, product marketing manager and most recently as product evangelist. Played a key role in scaling an early startup to $27M in revenue, 300+ employees, and 100M users.  </p>
-          <p className={["offset-by-three", "column", "about-message"].join(' ')}>Want to hire me? Send me an email.
-            <a target="_top" href="mailto:2nickels@gmail.com?subject=Just saw your portfolio online" className="underline">Click Here</a>
+          <p className={["offset-by-three", "column", "about-message"].join(' ')}>
+            <a target="_top" href="mailto:2nickels@gmail.com?subject=Just saw your portfolio online" className="underline">Click here to send me an email.</a>
           </p>
         </section>
 
