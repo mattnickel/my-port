391a68e4a764791b36806593f886451e9a045748
Matt Nickel <matt@nickelimages.com>
2019-10-18 14:27:08 -0700

    add resume page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Hex from '../assets/profile-hex.png';
+import Resumepg1 from '../assets/resume1.png';
+import Resumepg2 from '../assets/resume2.png';
 
 export default class Resume extends React.Component {
   render(){
     return (
-      <div>
-        <p>
-         This is Resume.
-        </p>
+      <div className="container">
+        <div className="resume_bkgd">
+          <img src={Resumepg1} className={["eight", "columns"].join(' ')} />
+        </div>
+        <div className="resume_bkgd">
+          <img src={Resumepg2} className={["eight", "columns"].join(' ')} />
+        </div>
       </div>
     );
   }
